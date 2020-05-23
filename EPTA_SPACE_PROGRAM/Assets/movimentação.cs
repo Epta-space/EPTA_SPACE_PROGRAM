b51d9f5a3490b79df65299f80151368f580cd737
Olavo <olavo1611@gmail.com>
2020-05-23 16:57:57 -0300

    Teste de movimentação
    Atualização para versão mais recente do unity + teste da nova movimentação

---
+++ EPTA_SPACE_PROGRAM/Assets/movimentação.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimentação : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float velocity;
+    private float screenWidth;
+    private Vector3 localScreenWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        screenWidth = Screen.width;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).position.x > screenWidth / 2)
+            {
+                if (transform.position.x <= screenWidth / 2)
+                {
+                    rb.velocity = Vector2.right * velocity;
+                }
+            }
+            else if (Input.GetTouch(0).position.x < screenWidth / 2)
+            {
+                if (transform.position.x >= -screenWidth / 2)
+                {
+                    rb.velocity = Vector2.left * velocity;
+                }
+            }
+        }
+
+        localScreenWidth = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+
+        if (transform.position.x >= localScreenWidth.x + 0.06 * localScreenWidth.x)
+        {
+
+            transform.position = new Vector3(-transform.position.x + 0.06f * localScreenWidth.x, transform.position.y, transform.position.z);
+
+        }
+        else if (transform.position.x <= -(localScreenWidth.x + 0.06 * localScreenWidth.x))
+        {
+
+            transform.position = new Vector3(-transform.position.x - 0.06f * localScreenWidth.x, transform.position.y, transform.position.z);
+        }
+    }
+}
