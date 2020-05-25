4c70d5597b66192a3a6ff4b886e2a6799e161a4c
Olavo <olavo1611@gmail.com>
2020-05-25 11:05:33 -0300

    Teste de movimentação pt.2
    Adição de bibliotecas faltantes no script de movimentação

--- EPTA_SPACE_PROGRAM/Assets/movimentação.cs
+++ EPTA_SPACE_PROGRAM/Assets/movimentação.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class movimentação : MonoBehaviour
 {
