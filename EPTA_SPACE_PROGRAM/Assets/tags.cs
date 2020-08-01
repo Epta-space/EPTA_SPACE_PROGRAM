419fbd4a0aeddf73e30efd3e06ab65b2faf546de
Olavo <olavo1611@gmail.com>
2020-08-01 01:08:41 -0300

    Colliders

--- EPTA_SPACE_PROGRAM/Assets/tags.cs
+++ EPTA_SPACE_PROGRAM/Assets/tags.cs
@@ -4,13 +4,95 @@ using UnityEngine;
 
 public class tags : MonoBehaviour
 {
-    public object alien{
-        set{
-        max_time_0 = 0;
-        velocidade = 0;
-        max_time = max_time_0;
-        pontos = 5;
-        }
-    }
-    public object[] objetos = {alien};
+    //Esses objetos não podem ser declarados dentro dessa classe: Procurar a dorme correta de declarar
+    // public object alien{
+    //     set{
+    //     max_time_0 = 0;
+    //     velocidade = 0;
+    //     max_time = max_time_0;
+    //     pontos = 5;
+    //     }
+    // }
+    // public object alien{
+    //     set{
+    //     max_time_0 = 0;
+    //     velocidade = 0;
+    //     max_time = max_time_0;
+    //     pontos = 5;
+    //     }
+    // }
+    // public object alien{
+    //     set{
+    //     max_time_0 = 0;
+    //     velocidade = 0;
+    //     max_time = max_time_0;
+    //     pontos = 5;
+    //     }
+    // }
+    // public object alien{
+    //     set{
+    //     max_time_0 = 0;
+    //     velocidade = 0;
+    //     max_time = max_time_0;
+    //     pontos = 5;
+    //     }
+    // }
+    // public object alien{
+    //     set{
+    //     max_time_0 = 0;
+    //     velocidade = 0;
+    //     max_time = max_time_0;
+    //     pontos = 5;
+    //     }
+    // }
+    // public object alien{
+    //     set{
+    //     max_time_0 = 0;
+    //     velocidade = 0;
+    //     max_time = max_time_0;
+    //     pontos = 5;
+    //     }
+    // }
+    // public object alien{
+    //     set{
+    //     max_time_0 = 0;
+    //     velocidade = 0;
+    //     max_time = max_time_0;
+    //     pontos = 5;
+    //     }
+    // }
+    // public object alien{
+    //     set{
+    //     max_time_0 = 0;
+    //     velocidade = 0;
+    //     max_time = max_time_0;
+    //     pontos = 5;
+    //     }
+    // }
+    // public object alien{
+    //     set{
+    //     max_time_0 = 0;
+    //     velocidade = 0;
+    //     max_time = max_time_0;
+    //     pontos = 5;
+    //     }
+    // }
+    // public object alien{
+    //     set{
+    //     max_time_0 = 0;
+    //     velocidade = 0;
+    //     max_time = max_time_0;
+    //     pontos = 5;
+    //     }
+    // }
+    // public object alien{
+    //     set{
+    //     max_time_0 = 0;
+    //     velocidade = 0;
+    //     max_time = max_time_0;
+    //     pontos = 5;
+    //     }
+    // }
+    
+    // public object[] objetos = {Alien,Balao};
 }
