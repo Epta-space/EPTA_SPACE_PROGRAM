b840c69e5555608ba3c9921470cf828d4e3b8e05
pedroguimelo <pedrogui.melo2000@gmail.com>
2021-01-28 22:21:51 -0300

    Inicio de Implementação do código Gravity

---
+++ Assets/Gravity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Gravity : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public int mass = 1;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.mass = mass;
+
+
+    }
+   
+
+}
