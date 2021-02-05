c1f7a8ca430d14906043446f77a883b1a9d30db8
pedroguimelo <pedrogui.melo2000@gmail.com>
2021-02-04 21:03:59 -0300

    Criação do script Destroyer_UI_Initial_background: script inicial

---
+++ Assets/scripts/incompleto/Destroyer_UI_inicial_background.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer_UI_Initial_background : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    public float timeRemaining = 10;
+    public int Get_phase() { return phase; }
+
+    void Update()
+    {
+        if (phase != 0)
+        {
+            // Destruir o UI_Initial_Background
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+    }
+}
