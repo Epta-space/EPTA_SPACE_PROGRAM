efd7b618fce3203cc6f4447c4f210481c832bd8c
pedroguimelo <pedrogui.melo2000@gmail.com>
2021-02-11 22:11:55 -0300

    modificação do script Destroyer_UI_inicial_background

--- Assets/scripts/incompleto/Destroyer_UI_inicial_background.cs
+++ Assets/scripts/incompleto/Destroyer_UI_inicial_background.cs
@@ -2,31 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Destroyer_UI_Initial_background : MonoBehaviour
+{  // Start is called before the first frame update
+    public dynamic game_manager;
 
-{
-    // Start is called before the first frame update
     void Start()
-    {
- 
-    }
+    {   
+        int fase = game_manager.GetComponent<Game_Manager>().Get_phase();
+        game_manager = GameObject.FindWithTag("GameManager");
 
-    public float timeRemaining = 10;
-    public int Get_phase() { return phase; }
+    }
 
     void Update()
     {
-        if (phase != 0)
+        float time = game_manager.GetComponent<Game_Manager>().Get_phase_time();
+
+        if (time > 10)
         {
-            // Destruir o UI_Initial_Background
-            if (timeRemaining > 0)
-            {
-                timeRemaining -= Time.deltaTime;
-            }
-            else
-            {
-                Destroy(this);
-            }
+        Destroy(this);
         }
-    }
+    
+    }  
 }
