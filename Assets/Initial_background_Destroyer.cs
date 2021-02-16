50da6eff2fd6b4ad3b778efd0d590241ee05ce9d
pedroguimelo <pedrogui.melo2000@gmail.com>
2021-02-15 22:43:13 -0300

    Missão Completa Destruição tela inicial

---
+++ Assets/Initial_background_Destroyer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Initial_background_Destroyer : MonoBehaviour
+{  // Start is called before the first frame update
+    private GameObject manager_object;
+
+    void Start()
+    {   
+        manager_object = GameObject.FindWithTag("Game_manager");
+ 
+    }
+
+    void Update()
+    {
+        int fase = manager_object.GetComponent<Game_Manager>().Get_phase();       
+        float time = manager_object.GetComponent<Game_Manager>().Get_phase_time();
+        Debug.Log(time);
+
+        if (time > 5)
+        {
+            Destroy(this.gameObject);
+            Debug.Log("Fim");
+        }
+    
+    }  
+}
