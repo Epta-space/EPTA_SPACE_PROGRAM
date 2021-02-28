043c2319879379646276eb0a06bf5caefddbbff3
Mateus <mateusnamikeze&@gmail.com>
2021-02-28 19:09:56 -0300

    problema no save volume

--- Assets/scripts/completo/audio_controls.cs
+++ Assets/scripts/completo/audio_controls.cs
@@ -1,26 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UIElements;
+using System.Collections;
 
 public class audio_controls: MonoBehaviour
 {
     // Reference to the scene audio control 
     private GameObject music_volume_control;
-
-    // makes the starting volume change in UI
-    public float volume;
-
+    private GameObject barra;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        // Acha o game audio principal, com a música do jogo via tag (GameObject Audio Source)
-        music_volume_control = GameObject.FindWithTag("audio_control");
         
-        // Declara o volume 
-        music_volume_control.GetComponent<AudioSource>().volume = volume;
+        music_volume_control = GameObject.FindWithTag("audio_control");
+        barra = GameObject.FindWithTag("music_control_1");
+
+        music_volume_control.GetComponent<AudioSource>().volume = float.Parse(GetComponent<save>().retornar_save("volume_musica"));
+        barra.GetComponent<Slider>().value = float.Parse(GetComponent<save>().retornar_save("volume_musica"));
+
+
+
 
     }
 
-    // Quando o valor do volume muda, atualiza o valor no Audio source da música
+    
     public void volume_controll(float volume){
         music_volume_control.GetComponent<AudioSource>().volume = volume;
+        GetComponent<save>().salvar(volume, "volume_musica");
     }
 }
