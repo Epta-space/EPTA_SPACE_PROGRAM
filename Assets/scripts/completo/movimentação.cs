40f274b8eb644c842a83299de8428f8c141c8d3b
olavocinacio <olavo1611@gmail.com>
2021-02-28 17:10:05 -0300

    Finalização do recuo_inicial - Desbloqueado o eixo y do player(avaliar necessidade de bloquear depois do recuo)

--- Assets/scripts/completo/movimentação.cs
+++ Assets/scripts/completo/movimentação.cs
@@ -10,7 +10,12 @@ public class movimentação : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         localScreenWidth = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
-        recuo_inicial();
+    }
+
+    void Update(){
+        if(transform.position.y >= -1.5){
+            rb.velocity = new Vector2( rb.velocity.x, -0.5f*speed);
+        }
     }
 
     void FixedUpdate()
@@ -50,12 +55,6 @@ public class movimentação : MonoBehaviour
                     rb.velocity = new Vector2( speed, rb.velocity.y);
                 }
             }
-        }
-    }
-
-    private void recuo_inicial(){
-        while(transform.position.y >= -3.5){
-            rb.velocity = new Vector2( 0, -speed);
         }
     }
 }
