cbd69d776f1bbccd83913172974279a19295bb00
Mateus <mateusnamikeze&@gmail.com>
2021-03-20 15:13:07 -0300

    tala de morte (SCRIPT)

---
+++ Assets/scripts/incompleto/tela_morte.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tela_morte : MonoBehaviour
+{
+    public GameObject tela;
+    void Start()
+    {
+        tela.SetActive(false);
+    }
+
+    public void tela_morte_ativar()
+    {
+        tela.SetActive(true);
+        Time.timeScale = 0;
+    }
+    
+    public void continuar()
+    {
+        Time.timeScale = 1;
+        Application.LoadLevel(Application.loadedLevel);
+    }
+    public void sair()
+    {
+        Application.Quit();
+    }
+}
