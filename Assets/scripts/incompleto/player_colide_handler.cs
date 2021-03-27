ebdacd5b02ad072016eee294bad47755cf74cebb
felipejoribeiro <feliperibeiro.ufu@gmail.com>
2021-03-27 17:02:01 -0300

    identificação da tag feita

--- Assets/scripts/incompleto/player_colide_handler.cs
+++ Assets/scripts/incompleto/player_colide_handler.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class player_colide_handler : MonoBehaviour
 {
+    // Vetor de obstáculos
+    private List<string> obstacle_list = new List<string>{"Nuv_1_0", "Nuv_1_1", "Nuv_3_0", "Balao"  , "Alien"  , "Met_1", "Met_2"  , "Met_3"  , "Sat_1", "Sat_2"  , "Sat_3"};
+
+
     // No momento em que ocorre a colisão ele chama essa função
     void OnCollisionEnter2D(Collision2D objeto_alvo)
     {
@@ -13,7 +17,10 @@ public class player_colide_handler : MonoBehaviour
     }
 
     private void ColisorHandler(string colidido){
-        Debug.Log(colidido);
+        if(obstacle_list.Contains(colidido)){
+            Debug.Log("bateu");
+
+        }
 
     }
 }
