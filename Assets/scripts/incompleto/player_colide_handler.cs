ecc121008f4e5e18cbbe726792cc150550814106
felipejoribeiro <feliperibeiro.ufu@gmail.com>
2021-03-27 16:51:42 -0300

    detecção da tag implementada

--- Assets/scripts/incompleto/player_colide_handler.cs
+++ Assets/scripts/incompleto/player_colide_handler.cs
@@ -6,14 +6,14 @@ using UnityEngine;
 public class player_colide_handler : MonoBehaviour
 {
     // No momento em que ocorre a colisão ele chama essa função
-    void OnCollisionEnter2D(Collision2D c)
+    void OnCollisionEnter2D(Collision2D objeto_alvo)
     {
         // Chama o gerenciador de colisões
-        ColisorHandler();
+        ColisorHandler(objeto_alvo.gameObject.tag);
     }
 
-    private void ColisorHandler(){
-
+    private void ColisorHandler(string colidido){
+        Debug.Log(colidido);
 
     }
 }
