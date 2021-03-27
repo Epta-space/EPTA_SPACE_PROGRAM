f77ff5851615e0933a0cd1c8195c221ac157d0f7
felipejoribeiro <feliperibeiro.ufu@gmail.com>
2021-03-27 16:45:25 -0300

    novo script criado

---
+++ Assets/scripts/incompleto/player_colide_handler.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_colide_handler : MonoBehaviour
+{
+    // No momento em que ocorre a colisão ele chama essa função
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        // Chama o gerenciador de colisões
+        ColisorHandler();
+    }
+
+    private void ColisorHandler(){
+
+
+    }
+}
